237fdfd7a2aa4eae69377d1f065ffd9442ed80c2
Martinique <me@martidolce.com>
2020-11-14 00:20:38 -0500

    Objectives Understand how to control the return value of a method that uses yield. Gain more practice with the yield keyword and blocks. Gain a deeper understanding of the common iterator, #collect.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,17 @@
-require 'pry'
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(%w[Tim Tom Jim]) { |name| "Hi, #{name}" }
